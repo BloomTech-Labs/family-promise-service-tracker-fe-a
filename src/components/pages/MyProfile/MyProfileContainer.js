c5d327c28582ec667779e6d88791dd84073de2b1
Hussain Ali <ali.415code@gmail.com>
2021-04-11 14:13:17 -0700

    Profile Page Shows Current User Info

--- src/components/pages/MyProfile/MyProfileContainer.js
+++ src/components/pages/MyProfile/MyProfileContainer.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import RenderMyProfile from './RenderMyProfile';
+import { useOktaAuth } from '@okta/okta-react';
 import { TabletHeader } from '../../common/index';
+import { axiosWithAuth } from '../../../utils/axiosWithAuth';
+import './profile.css';
 
-function MyProfileContainer() {
+function MyProfileContainer({ LoadingOutlined }) {
+  const { authState, authService } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(false);
+  const [curUser, setCurUser] = useState(false);
+  // eslint-disable-next-line
+  const [memoAuthService] = useMemo(() => [authService], []);
+
+  useEffect(() => {
+    let isSubscribed = true;
+
+    memoAuthService
+      .getUser()
+      .then(info => {
+        // if user is authenticated we can use the authService to snag some user info.
+        // isSubscribed is a boolean toggle that we're using to clean up our useEffect.
+        if (isSubscribed) {
+          setUserInfo(info.sub);
+        }
+      })
+      .catch(err => {
+        isSubscribed = false;
+        return setUserInfo(null);
+      });
+    return () => (isSubscribed = false);
+  }, [memoAuthService]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`api/profile/${userInfo}`)
+      .then(res => {
+        setCurUser(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [userInfo]);
+
+  console.log(curUser);
   return (
     <div>
       <TabletHeader />
-      <RenderMyProfile />
+      {authState.isAuthenticated && !curUser && (
+        <LoadingOutlined className="loader" />
+      )}
+      {authState.isAuthenticated && curUser && (
+        <RenderMyProfile
+          curUser={curUser}
+          LoadingOutlined={LoadingOutlined}
+          authState={authState}
+        />
+      )}
     </div>
   );
 }
--- src/components/pages/MyProfile/RenderMyProfile.js
+++ src/components/pages/MyProfile/RenderMyProfile.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Avatar } from 'antd';
 
-function RenderMyProfile(props) {
+function RenderMyProfile({ curUser }) {
   return (
     <div>
-      <h1>My Profile</h1>
+      <h2>Welcome, {curUser.name}</h2>
+      <div className="avatar-ctn">
+        <Avatar size={200} src={curUser.avatarUrl} />
+      </div>
+      <h3>Email: {curUser.email}</h3>
+      <h3>Role: {curUser.role}</h3>
     </div>
   );
 }
