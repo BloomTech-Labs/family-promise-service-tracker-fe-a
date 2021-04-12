0b3245a3e68d61b4e81eccc365f889f1ddbe7804
RubesWorld <hi@rubesworld.live>
2021-04-12 15:14:21 -0700

    Added Folder in Services and created barebones form

--- src/components/pages/Services/RenderServicesPage.js
+++ src/components/pages/Services/RenderServicesPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+//component imports
+import RenderServiceForm from './form-service/renderServiceForm';
 
 function RenderServicesPage() {
   return (
-    <div>
-      <h1>Services</h1>
-    </div>
+    <>
+      <div>
+        <h1>Services</h1>
+      </div>
+      <RenderServiceForm />
+    </>
   );
 }
 
