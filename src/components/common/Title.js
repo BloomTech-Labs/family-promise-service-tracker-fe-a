c583c3a6dbac0f1c6172a65931945162f2b4b3bd
RubesWorld <hi@rubesworld.live>
2021-04-17 18:34:49 -0700

    Added comment to Title component

--- src/components/common/Title.js
+++ src/components/common/Title.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Typography } from 'antd';
 
 function TitleComponent({ TitleText }) {
+  //Be sure to pass the TitleText in quotes when you render component
   const { Title } = Typography;
   return (
     <div className="title-container">
