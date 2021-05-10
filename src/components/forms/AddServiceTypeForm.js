b42621885c2372599820769bbc0b8250bdc37b0c
danielwright1207 <daniel.wright1207@gmail.com>
2021-05-10 11:02:00 -0600

    added unit, quantity, value, date/time, location, and status fields to the add service type form

--- src/components/forms/AddServiceTypeForm.js
+++ src/components/forms/AddServiceTypeForm.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Modal, Form, Input, Select } from 'antd';
+import {
+  Form,
+  Input,
+  Select,
+  InputNumber,
+  DatePicker,
+  TimePicker,
+  Modal,
+} from 'antd';
+// import { Modal, Form, Input, Select } from 'antd';
 
 const programs = ['Prevention', 'After Care', 'Sheltering'];
 
@@ -45,24 +54,118 @@ function AddServiceTypeForm({ onCreate, onCancel, visible }) {
             ]}
           >
             <Input />
+            <Form.Item
+              name="type"
+              label="Program"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the program type',
+                },
+              ]}
+            >
+              <Select size="large" placeholder="Select Program">
+                {programs.map(item => (
+                  <Select.Option key={item}>{item}</Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+            <Form.Item label="Service Description" name="description">
+              <Input.TextArea showCount maxLength={240} />
+            </Form.Item>
+            <Form.Item label="Unit" name="unit">
+              <Input.TextArea showCount maxLength={240} />
+            </Form.Item>
+            <Form.Item label="Quantity" name="quantity">
+              <Input.TextArea showCount maxLength={240} />
+            </Form.Item>
+            <Form.Item label="Value" name="value">
+              <Input.TextArea showCount maxLength={240} />
+            </Form.Item>
+            <Form.Item label="Provider" name="provider">
+              <Input.TextArea showCount maxLength={240} />
+            </Form.Item>
+            <Form.Item label="Recipient" name="recipient">
+              <Input.TextArea showCount maxLength={240} />
+            </Form.Item>
+            <div className="date-time-wrapper">
+              <Form.Item
+                label="Date"
+                name="date"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Enter Date',
+                  },
+                ]}
+              >
+                <DatePicker size="large" />
+              </Form.Item>
+              <Form.Item
+                label="Time"
+                className="time-input"
+                name="time"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Select Time',
+                  },
+                ]}
+              >
+                <TimePicker use12Hours format="h:mm a" size="large" />
+              </Form.Item>
+            </div>
           </Form.Item>
+
           <Form.Item
-            name="type"
-            label="Program"
+            label="Address"
+            name="Address"
             rules={[
               {
                 required: true,
-                message: 'Please input the program type',
+                message: 'Please enter the address',
               },
             ]}
           >
-            <Select size="large" placeholder="Select Program">
-              {programs.map(item => (
-                <Select.Option key={item}>{item}</Select.Option>
-              ))}
-            </Select>
+            <Input placeholder="Enter City" size="large" />
           </Form.Item>
           <Form.Item
+            label="City"
+            name="city"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter the address',
+              },
+            ]}
+          >
+            <Input placeholder="Enter State" size="large" />
+          </Form.Item>
+          <Form.Item
+            label="State"
+            name="state"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter the address',
+              },
+            ]}
+          >
+            <Input placeholder="Enter Zipcode" size="large" />
+          </Form.Item>
+          <Form.Item
+            label="Zip Code"
+            name="zipcode"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter the ZipCode',
+              },
+            ]}
+          >
+            <Input placeholder="Enter address" size="large" />
+          </Form.Item>
+          {/* <Form.Item
             name="type"
             label="Employees"
             rules={[
@@ -77,9 +180,22 @@ function AddServiceTypeForm({ onCreate, onCancel, visible }) {
                 <Select.Option key={item}>{item}</Select.Option>
               ))}
             </Select>
-          </Form.Item>
-          <Form.Item label="Service Description" name="description">
-            <Input.TextArea showCount maxLength={240} />
+          </Form.Item> */}
+          <Form.Item
+            name="type"
+            label="Status"
+            rules={[
+              {
+                required: true,
+                message: 'Please select service status',
+              },
+            ]}
+          >
+            <Select size="large" placeholder="Select status">
+              {programs.map(item => (
+                <Select.Option key={item}>{item}</Select.Option>
+              ))}
+            </Select>
           </Form.Item>
         </Form>
       </Modal>
