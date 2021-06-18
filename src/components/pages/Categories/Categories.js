5b79e6ec326848f3f3ffa53bba150abe48f0fb7e
robertjallen <bobbidigi34@gmail.com>
2021-06-17 21:53:18 -0400

    add button

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from 'antd';
 
 export default function Categories() {
   return (
     <div>
       <h1>Categories</h1>
+      <Button type="primary" onClick={() => {}}>
+        Add Category
+      </Button>
     </div>
   );
 }
