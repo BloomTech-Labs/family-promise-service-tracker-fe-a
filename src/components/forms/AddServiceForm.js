b3d8ebfdaa0ec983996b4eec6f91fa7619f0bbc9
MichaelHabermas <michaelghabermas@gmail.com>
2021-08-10 15:08:21 -0400

    recipients now displayed in create service log

--- src/components/forms/AddServiceForm.js
+++ src/components/forms/AddServiceForm.js
@@ -60,7 +60,9 @@ function AddServiceForm({
             <Select size="large" placeholder="Select Recipient">
               {recipients.map(recipient => (
                 <Select.Option key={recipient.id} value={recipient.id}>
-                  {recipient.first_name + ' ' + recipient.last_name}
+                  {recipient.recipient_first_name +
+                    ' ' +
+                    recipient.recipient_last_name}
                 </Select.Option>
               ))}
             </Select>
