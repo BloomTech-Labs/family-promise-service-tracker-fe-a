b6bdd8c7aba2f537941087781a30d49d712162d3
ahmedseragcodes <ahmedserag1@gmail.com>
2021-08-12 17:16:31 -0400

    added styling to eligibility dashboard

--- src/components/pages/Dashboard/DashboardHouseholdEligibility.js
+++ src/components/pages/Dashboard/DashboardHouseholdEligibility.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../../../styles/Dashboard.scss';
 
 export default function EligibilityDashboard() {
   const [eligible, setEligible] = useState('');
   const [householdId, setHouseholdId] = useState(null);
+
+  const handleChange = () => {};
 
   const getEligibility = household_id => {
     axios.get(`/${household_id}`).then(res => {});
@@ -12,10 +15,23 @@ export default function EligibilityDashboard() {
 
   return (
     <div className="eligibilityDashboardContainer">
-      <h2>Eligibility</h2>
+      <h1>Eligibility</h1>
+      <div className="eligibilityForm">
+        <form>
+          <label htmlFor="householdId">
+            <input
+              name="householdId"
+              id="householdId"
+              placeholder="Enter A Household"
+              value={householdId}
+            />
+          </label>
+          <button>Submit</button>
+        </form>
+      </div>
       <div className="eligibilityResults">
         <p>
-          Eligible For Residential Assistance:{' '}
+          Eligible For Residential Assistance:
           {eligible.resident_assistance_eligibility}
         </p>
         <p>
