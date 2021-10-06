b3ce420d41de36c9c0c7da3b98e9896db6b4a2a3
Jim Liberi <ptanalytics@gmail.com>
2021-10-06 18:09:33 -0400

    removed unused and replaced code

--- src/components/pages/Dashboard/ServiceMap.js
+++ src/components/pages/Dashboard/ServiceMap.js
@@ -1,10 +1,5 @@
 import * as React from 'react';
 import { useState, useRef } from 'react';
-// import ReactMapGL, { Marker, FlyToInterpolator } from 'react-map-gl';
-
-// import purplePing from '../../../assets/purplePing.png';
-// import yellowPing from '../../../assets/yellowPing.png';
-// import bluePing from '../../../assets/bluePing.png';
 import ReactMapGL, { Source, Layer } from 'react-map-gl';
 import {
   clusterLayer,
@@ -12,11 +7,7 @@ import {
   unclusteredPointLayer,
 } from './layers';
 import './styles.css';
-// Map for dashboard:
 
-// Must have a token setup in a .env.local file
-// with the following format for map to render
-// REACT_APP_MAPBOX_TOKEN = "Token string"
 export default function ServiceMap(props) {
   const [viewport, setViewport] = useState({
     latitude: 47.658779,
@@ -26,72 +17,11 @@ export default function ServiceMap(props) {
     pitch: 0,
   });
 
-  // Logic for reset button
-  // const resetViewport = () => {
-  //   setViewport({
-  //     ...viewport,
-  //     latitude: 47.658779,
-  //     longitude: -117.426048,
-  //     zoom: 11,
-  //     transitionDuration: 500,
-  //     transitionInterpolator: new FlyToInterpolator(),
-  //   });
-  // };
-
   const mapRef = useRef(null);
-
-  // Marker generator taking in data from props currently
-  // Currently hardcoded needs conditional logic to display different markers dynamically based on program type - DONE
-  // if  else if  else - style={{background: dot.program === "Prevention" ? '#472D5B' : dot.program === "Shelter Support" ? '#FEC357' : '#006FBA'}}> - DONE
-  // .mapMarkerStylezoomedOut is for styling of zoomed out wide view of map clusters - DONE
-  // .mapMarkerStyle is for zoomed in view styling of map services - DONE
-
-  // const markers = React.useMemo(
-  //   () =>
-  //     props.data.map(dot => (
-  //       <Marker
-  //         key={dot.program}
-  //         longitude={dot.longitude}
-  //         latitude={dot.latitude}
-  //       >
-  //         {/* <img src={yellowPing} /> */}
-
-  //         <div
-  //           className="mapMarkerStylezoomedOut"
-  //           style={{
-  //             background:
-  //               dot.program === 'Prevention'
-  //                 ? '#472D5B'
-  //                 : dot.program === 'Shelter Support'
-  //                 ? '#FEC357'
-  //                 : '#006FBA',
-  //           }}
-  //         ></div>
-  //       </Marker>
-  //     )),
-  //   [props.data]
-  // );
 
   const onClick = event => {
     const feature = event.features[0];
     console.log('features', feature);
-    // const clusterId = feature.properties.id;
-
-    // const mapboxSource = mapRef.current.getMap().getSource('earthquakes');
-
-    // mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
-    //   if (err) {
-    //     return;
-    //   }
-
-    //   setViewport({
-    //     ...viewport,
-    //     longitude: feature.geometry.coordinates[0],
-    //     latitude: feature.geometry.coordinates[1],
-    //     zoom,
-    //     transitionDuration: 500
-    //   });
-    // });
   };
 
   return (
@@ -120,7 +50,6 @@ export default function ServiceMap(props) {
             id="programClusters"
             type="geojson"
             data={props.data}
-            // data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
             cluster={true}
             clusterMaxZoom={14}
             clusterRadius={50}
@@ -135,13 +64,3 @@ export default function ServiceMap(props) {
     </div>
   );
 }
-
-// if (dot.program === "Prevention"){
-//   dot.style.backgroundColor = '#472D5B'
-// }
-// if (dot.program === "Shelter Support"){
-//   dot.style.backgroundColor = '#FEC357'
-// }
-// else {
-//   dot.style.backgroundColor = '#006FBA'
-// }
