396b062b5bd7088bbaa1dbf89ba73547e07b1fba
Andrew Castillo <andrewcast32@gmail.com>
2021-10-07 17:00:44 -0700

    Added Dummy Json file for our Popup and marker functionability, having issue with using geojson prop. Changed to class function for now

---
+++ src/components/pages/Dashboard/City-Info.js
@@ -0,0 +1,28 @@
+// This component renders the dummy info during the Pop-up on Click
+import * as React from 'react';
+
+function CityInfo(props) {
+  const { info } = props;
+  const displayName = `${info.city}, ${info.state}`;
+
+  return (
+    <div>
+      <div>
+        {displayName} |{' '}
+        <a
+          target="_new"
+          href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${displayName}`}
+        >
+          Wikipedia
+        </a>
+      </div>
+      <img
+        width={240}
+        src={info.image}
+        alt="image of city we are gathering data from"
+      />
+    </div>
+  );
+}
+
+export default React.memo(CityInfo);
--- src/components/pages/Dashboard/ServiceMap.js
+++ src/components/pages/Dashboard/ServiceMap.js
@@ -1,78 +1,116 @@
 import * as React from 'react';
-import { useState, useRef } from 'react';
-import ReactMapGL, { Source, Layer } from 'react-map-gl';
+// import { useState, useRef } from 'react';
+import ReactMapGL, {
+  Marker,
+  Source,
+  Layer,
+  Popup,
+  NavigationControl,
+} from 'react-map-gl';
 import {
   clusterLayer,
   clusterCountLayer,
   unclusteredPointLayer,
 } from './layers';
 import './styles.css';
+import CITIES from './cities.json';
+import blueping from '../../../assets/bluePing.png';
+import CityInfo from './City-Info';
 
-export default function ServiceMap(props) {
-  const [viewport, setViewport] = useState({
-    latitude: 47.658779,
-    longitude: -117.426048,
-    zoom: 11,
-    bearing: 0,
-    pitch: 0,
-  });
-
-  const mapRef = useRef(null);
-
-  const onClick = event => {
-    const feature = event.features[0];
-    console.log('features', feature);
+export default class ServiceMap extends React.Component {
+  state = {
+    viewport: {
+      latitude: 47.658779,
+      longitude: -117.426048,
+      zoom: 11,
+      bearing: 0,
+      pitch: 0,
+    },
+    popupInfo: null,
   };
-  // Currently hardcoded needs conditional logic to display
-  // different markers dynamically based on program type (line 44)
-  // See Figma prototype for example
-  const markers = React.useMemo(
-    () =>
-      props.data.map(dot => (
-        <Marker longitude={dot.longitude} latitude={dot.latitude}>
-          <img src={yellowPing} />
-        </Marker>
-      )),
-    [props.data]
-  );
 
-  return (
-    <div className="service-map">
-      {/* <ReactMapGL
-        {...viewport}
-        onViewportChange={nextViewport => setViewport(nextViewport)}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+  mapRef = React.createRef(null);
+
+  updateViewport = viewport => {
+    this.setState({ viewport });
+  };
+
+  // send the info we gather to the popup info stuff
+  // renderCityMarker is also setting the state of popupInfo to the entire city's data
+  renderMarker = location => {
+    return (
+      <Marker
+        key={location}
+        longitude={location.longitude}
+        latitude={location.latitude}
       >
-        {markers}
-      </ReactMapGL>
-      <button onClick={resetViewport}>Reset</button> */}
-      <>
-        <ReactMapGL
-          {...viewport}
-          width="100vw"
-          height="50vh"
-          mapStyle="mapbox://styles/mapbox/light-v9"
-          onViewportChange={setViewport}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          interactiveLayerIds={[clusterLayer.id]}
-          onClick={onClick}
-          ref={mapRef}
+        <img
+          src={blueping}
+          onClick={() => this.setState({ popupInfo: location })}
+          alt="blue marker for location"
+        />
+      </Marker>
+    );
+  };
+
+  // with the data from the state, we can now have our popup show our CityInfo component that handles
+  // our layout for the popup
+  renderPopup() {
+    const { popupInfo } = this.state;
+
+    return (
+      popupInfo && (
+        <Popup
+          tipSize={5}
+          anchor="bottom"
+          longitude={popupInfo.longitude}
+          latitude={popupInfo.latitude}
+          // to make it easier on tablet users, we can click anywhere and it'll auto close the popup
+          // otherwise we can set this to true to have it close only when we press the x
+          // closeOnClick={false}
+          onClose={() => this.setState({ popupInfo: null })}
         >
-          <Source
-            id="programClusters"
-            type="geojson"
-            data={props.data}
-            cluster={true}
-            clusterMaxZoom={14}
-            clusterRadius={50}
+          <CityInfo info={popupInfo} />
+        </Popup>
+      )
+    );
+  }
+
+  render() {
+    const { viewport } = this.state;
+
+    return (
+      <div className="service-map">
+        <>
+          <ReactMapGL
+            {...viewport}
+            width="100vw"
+            height="50vh"
+            mapStyle="mapbox://styles/mapbox/light-v9"
+            onViewportChange={this.updateViewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            interactiveLayerIds={[clusterLayer.id]}
+            ref={this.mapRef}
           >
-            <Layer {...clusterLayer} />
-            <Layer {...clusterCountLayer} />
-            <Layer {...unclusteredPointLayer} />
-          </Source>
-        </ReactMapGL>
-        {/* <button onClick={resetViewport}>Reset</button>  */}
-      </>
-    </div>
-  );
+            {/* we map through the dummy data so that we can render each using our marker function */}
+            {CITIES.map(this.renderMarker)}
+            {this.renderPopup()}
+            <Source
+              id="programClusters"
+              type="geojson"
+              data={this.props.data}
+              cluster={true}
+              clusterMaxZoom={14}
+              clusterRadius={50}
+            >
+              <Layer {...clusterLayer} />
+              <Layer {...clusterCountLayer} />
+              <Layer {...unclusteredPointLayer} />
+            </Source>
+            <NavigationControl />
+          </ReactMapGL>
+        </>
+      </div>
+    );
+  }
 }
