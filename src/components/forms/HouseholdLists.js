71fcfe034789db6facabe18a44df4c3ce7dcbb68
abemm7787 <74838547+abemm7787@users.noreply.github.com>
2021-10-22 12:36:10 -0700

    updated buttons

--- src/components/forms/HouseholdLists.js
+++ src/components/forms/HouseholdLists.js
@@ -1,30 +1,32 @@
 // container
-import React, { Component } from 'react';
-import { useState } from 'react';
+import React, { useState, Component, useReducer } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { selectUser } from '../../state/actions/householdeligibilityActions';
 import 'antd/dist/antd.css';
 import { Button } from 'antd';
-
 import logo from '../../../src/assets/logo.png';
 // This component provides an option for the user to click and observe disparate eligibility for the surrounding area.
 
 function HouseholdList(props) {
+  const createListItems = () => {
+    return props.services.map(service => {
+      return (
+        <Button
+          type="primary"
+          key={service.id}
+          onClick={() => props.selectUser(service)}
+        >
+          {service.Prevention} {service.Shelter} {service.Aftercare}
+        </Button>
+      );
+    });
+  };
+
   return (
-    <div>
-      {console.log(props.services.services)}
-      {props.services.map(service => {
-        return (
-          <div key={service.id} onClick={() => props.selectUser(service)}>
-            <div className="serviceButtons">
-              <h3 id="yellowButton">{service.Prevention}</h3>
-              <h3 id="blueButton">{service.Shelter}</h3>
-              <h3 id="greenButton">{service.Aftercare}</h3>
-            </div>
-          </div>
-        );
-      })}
+    <div className="">
+      <div className="serviceButton">{createListItems()}</div>
+      );
     </div>
   );
 }
--- src/state/actions/householdeligibilityActions.js
+++ src/state/actions/householdeligibilityActions.js
@@ -4,7 +4,7 @@ export const USER_SELECTED = 'USER_SELECTED';
 export function toggleEditing(services) {
   return { type: 'TOGGLE_EDITING', payload: services };
 }
-//The action creator generates a new action for the user to discerned once a click has been made.
+//The action creator generates a new action once a click has been made.
 export function selectUser(user) {
   return {
     type: 'USER_SELECTED',
